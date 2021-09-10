89b6373be388af7152c91c49ed7261634cb5fddf
CaioNunes <caiofelipe147@gmail.com>
2021-09-09 22:09:37 -0300

    Criando base de dados de armas

---
+++ src/databases/armas.pl
@@ -0,0 +1,12 @@
+% Indice, Nome, Dica
+
+arma(1, 'CHICOTE', 'Arma - Conhecida desde a idade de cristo. Essa já causou estrago.').
+arma(2, 'AGULHAS_ENVENENADAS', 'Arma - Essa é bem incomum. Me lembro de uma música que envolvia algo como cobras...').
+arma(3, 'CORRENTE_ELETRICA', 'Arma - Hollywood já usou isso como origem de super heróis. Mas parece que não funciona assim na vida real.').
+arma(4, 'RADIACAO', 'Arma - Silencioso, lento, mortal... Mas até seu microondas pode contribuir.').
+arma(5, 'CHUTE', 'Arma - Alguns atores bem famosos ficariam orgulhosos.').
+arma(6, 'FURADEIRA', 'Arma - Mas que diabos... Ninguém ouviu essa barulheira não ?').
+arma(7, 'MACHADO', 'Arma - Brutal... Ele é o que, um açougueiro ?').
+arma(8, 'ARAME', 'Arma - Versátil, discreto, metálico. Seca roupas e tira vidas.').
+arma(9, 'TROFÉU', 'Arma - Uma grande conquista. Mas parece que alguém não queria tanto assim, até forçou outro a ficar com ele.').
+arma(10, 'PREGO', 'Arma - Tão pequeno e inofensivo... Mas bata com força, e já era.').
