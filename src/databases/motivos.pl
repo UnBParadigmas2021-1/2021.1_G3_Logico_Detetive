ffe525739ae116be567d894e1449f5673b2dec47
CaioNunes <caiofelipe147@gmail.com>
2021-09-09 22:10:44 -0300

    criando base de dados de motivos

---
+++ src/databases/motivos.pl
@@ -0,0 +1,7 @@
+% Indice, Nome, Dica
+
+motivo(1, 'SER_UNICO_HERDEIRO', 'Motivo - Aquilo que move o mundo. E faz pessoas serem assassinadas.').
+motivo(2, 'DESCOBRIU_TRAICAO_CONJUGAL', 'Motivo - Não sei se devemos ficar triste pela morte desse. Afinal, quem ficaria feliz com essa punhalada pelas costas ?').
+motivo(3, 'TEVE_SUA_EMPRESA_ROUBADA', 'Motivo - Família, amigos, colegas, conhecidos... Negócios SEMPRE a parte.').
+motivo(4, 'NAO_ACEITOU_O_TERMINO', 'Motivo - O fim nunca é fácil. Mas não precisava ser tão difícil...').
+motivo(5, 'INVEJA_DO_SUCESSO', 'Motivo - É bom ver os outros tão felizes. As vezes.').
