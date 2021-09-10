470c64a3f29115824b86f5c2736b3be38b74b28a
CaioNunes <caiofelipe147@gmail.com>
2021-09-09 22:10:01 -0300

    Criando base de dados de locais

---
+++ src/databases/locais.pl
@@ -0,0 +1,7 @@
+% Indice, Nome, Dica
+
+local(1, 'BIBLIOTECA', 'Local - Como é que tanta gente pode gostar desse lugar. É mórbido e... silencioso demais.').
+local(2, 'HOSPITAL', 'Local - A primeira casa de muita gente. Isso é, se você tiver sorte.').
+local(3, 'IGREJA', 'Local - O lugar mais conhecido e frequentado do mundo. Como é possível ?').
+local(4, 'ESCRITORIO', 'Local - Um nome chique pra um lugar estiloso. Ah, o século 21...').
+local(5, 'BAR', 'Local - Pra alguns, mais prazeroso que a própria casa.').
