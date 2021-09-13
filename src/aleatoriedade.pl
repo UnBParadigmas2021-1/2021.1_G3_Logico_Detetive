dabf2564b67e8c8b1a59973480cb9a065553cc88
Eugenio Sales <eugenio.sales@outlook.com>
2021-09-13 12:49:58 -0300

    Adicionando dinâmica do menu de local
    Co-authored-by: wallacybraz <wallacy.braz@live.com>

--- src/aleatoriedade.pl
+++ src/aleatoriedade.pl
@@ -22,14 +22,18 @@ solucao_aleatoria :- nl,
     arma(IndiceArma, Arma, _),
     write('IndiceArma:'), write(IndiceArma),
     
-    % Armazenamento da Variável
+    % Armazenamento da Variável Local
     arma(IndiceArma, Arma, Dica),
     recorda(idArma, IndiceArma),
     recorda(dica_arma, Dica),
     
     nl,
     random_between(1, 5, IndiceLocal),
-    local(IndiceLocal, Local, _),
+    
+    local(IndiceLocal, Local, DicaLocal),
+    recorda(idLocal, IndiceLocal),
+    recorda(dica_local, DicaLocal),
+
     write('IndiceLocal:'), write(IndiceLocal),
     nl,
     random_between(1, 5, IndiceMotivo),
@@ -39,8 +43,7 @@ solucao_aleatoria :- nl,
     asserta(solucao(Arma, Local, Motivo)),
     solucao(RespostA, RespostB, RespostC),
     write('Solucao Final:'), write(RespostA), write('-'), write(RespostB), write('-'), write(RespostC), nl,
-    write(counter).
-
+    menuArma.
 
 menuArma :- nl,
     write('Dica da arma do crime: '), recorded(dica_arma, X),
@@ -75,7 +78,7 @@ menuArma :- nl,
     
     % Verificando a Resposta    
     (solucao(RespostaUsuario,_,_) -> 
-        nl, write('Chamar outro menu (Motivo ou Local)'), nl;
+        nl, write('Chamar outro menu (Motivo ou Local)'), tty_clear, menuLocal;
         
         % Lógica dos Valores referentes ao Incrementador
         recorded(contador, CONTADOR),
@@ -85,4 +88,51 @@ menuArma :- nl,
         (CONTADOR_ATUAL < 5 -> 
             tty_clear, write('ERROU!! Você possui '), write(CONTADOR_ATUAL/4) ,write(' tentativas'), menuArma ; gameOver)).
 
+
+menuLocal :- nl,
+    recorded(contador, CONTADOR_ATUAL),
+    write('Você ainda possui '), write(CONTADOR_ATUAL/4) ,write(' tentativas'),nl,
+    write('Dica do local do crime: '), recorded(dica_local, Y),
+    write(Y),
+    write('\nChute uma das opções abaixo:\n'),
+    
+    recorded(idLocal, OpcaoCorretaB),
+    
+    % Mapeamento das Alternativas
+    local(OpcaoCorretaB,NomeLocalCerto,_),
+    random(1, 5, Opcao1), local(Opcao1,LocalErrado1,_),
+    random(1, 5, Opcao2),local(Opcao2,LocalErrado2,_),
+    random(1, 5, Opcao3), local(Opcao3,LocalErrado3,_),
+
+    random_permutation([LocalErrado1, LocalErrado2, LocalErrado3, NomeLocalCerto],AlternativasLocal),
+
+    % Setando Alternativas
+    nth1(1,AlternativasLocal,A),
+    nth1(2,AlternativasLocal,B),
+    nth1(3,AlternativasLocal,C),
+    nth1(4,AlternativasLocal,D),
+    write('=======================================\n'),
+    write(A), write('\n'),
+    write(B), write('\n'),
+    write(C), write('\n'),
+    write(D), write('\n'),
+    write('======================================='), nl,
+    write('Digite uma das Opções acima: '), 
+    
+    % Leitura de Input do Usuário
+    read_line_to_codes(user_input,Cs), atom_codes(RespostaUsuario, Cs), atomic_list_concat(L, ' ', RespostaUsuario),
+    write(RespostaUsuario),
+    
+    % Verificando a Resposta    
+    (solucao(_,RespostaUsuario,_) -> 
+        nl, write('Chamar outro menu (Motivo)'), nl;
         
+        % Lógica dos Valores referentes ao Incrementador
+        
+        recorded(contador, CONTADOR_LOC),
+        nl, nl, write(CONTADOR_LOC), nl, nl,
+        incrementador(CONTADOR_LOC, CONTADOR_LOCAL),
+        recorda(contador, CONTADOR_LOCAL),
+
+        (CONTADOR_LOCAL < 5 -> 
+            write('ERROU!! Você possui '), write(CONTADOR_LOCAL/4) ,write(' tentativas') ,tty_clear, menuLocal; gameOver)).
